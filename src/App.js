716cbd3b735abe25f3d0b4a4db3e22c7cc571ea1
shivanshupareek <97258879+shivanshupareek@users.noreply.github.com>
2024-05-13 20:46:39 +1000

    temperature react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [slider, setSlider] = useState(23);
+  const [fahrenheitSlider, setFahrenheitSlider] = useState(78);
+  const [showCelsius, setShowCelsius] = useState(true);
+
+  const toggleTemperature = () => {
+    setShowCelsius((prev) => !prev);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="temperature">
+        <div className="conversionBackground">
+          <button
+            type="button"
+            className={`celcius ${showCelsius ? "active" : ""}`}
+            onClick={toggleTemperature}
+          >
+            &#8451;
+          </button>
+          <button
+            type="button"
+            className={` fahrenheit ${!showCelsius ? "active" : ""}`}
+            onClick={toggleTemperature}
+          >
+            &#8457;
+          </button>
+        </div>
+        {showCelsius ? (
+          <Celsius slider={slider} setSlider={setSlider} />
+        ) : (
+          <Fahrenheit
+            fahrenheitSlider={fahrenheitSlider}
+            setFahrenheitSlider={setFahrenheitSlider}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Celsius({ slider, setSlider }) {
+  return (
+    <div>
+      <div className="card">
+        <div
+          className={`
+          ${slider <= -1 ? "temperatureMinus" : " "}
+          ${slider >= 0 ? "temperatureZero" : " "} 
+          ${slider >= 10 ? "temperatureTen" : " "} 
+          ${slider >= 20 ? "temperatureTwenty" : " "} 
+          ${slider >= 30 ? "temperatureThirty" : " "}
+          ${slider >= 40 ? "temperatureForty" : " "} 
+          ${slider >= 50 ? "temperatureFifty" : " "}
+          `}
         >
-          Learn React
-        </a>
-      </header>
+          <h1>{Number(slider)}&#8451;</h1>
+        </div>
+        <div>
+          <input
+            type="range"
+            max={60}
+            min={-20}
+            value={slider}
+            onChange={(e) => setSlider(Number(e.target.value))}
+            className="slider"
+          />
+        </div>
+        <div className="controls">
+          <div className="buttons">
+            <button type="button" onClick={() => setSlider(slider - 1)}>
+              {" - "}
+            </button>
+          </div>
+          <div className="buttons">
+            <button type="button" onClick={() => setSlider(slider + 1)}>
+              {" + "}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Fahrenheit({ fahrenheitSlider, setFahrenheitSlider }) {
+  return (
+    <div className="card">
+      <div
+        className={`
+            ${fahrenheitSlider <= 30 ? "temperatureMinus" : " "}
+            ${fahrenheitSlider >= 32 ? "temperatureZero" : " "}
+            ${fahrenheitSlider >= 50 ? "temperatureTen" : " "}
+            ${fahrenheitSlider >= 68 ? "temperatureTwenty" : " "}
+            ${fahrenheitSlider >= 86 ? "temperatureThirty" : " "}
+            ${fahrenheitSlider >= 104 ? "temperatureForty" : " "}
+            ${fahrenheitSlider >= 122 ? "temperatureFifty" : " "}
+            `}
+      >
+        <h1>{Number(fahrenheitSlider)}&#8457;</h1>
+      </div>
+      <div>
+        <input
+          type="range"
+          max={140}
+          min={-4}
+          value={fahrenheitSlider}
+          onChange={(e) => setFahrenheitSlider(Number(e.target.value))}
+          className="slider"
+        />
+      </div>
+      <div className="controls">
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={() => setFahrenheitSlider(fahrenheitSlider - 1)}
+          >
+            {" - "}
+          </button>
+        </div>
+        <div className="buttons">
+          <button
+            type="button"
+            onClick={() => setFahrenheitSlider(fahrenheitSlider + 1)}
+          >
+            {" + "}
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
